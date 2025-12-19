28eb4b18c2cd09647d098922779d843353a462f7
MajidHJ <53868183+MajidHJ@users.noreply.github.com>
2025-12-19 11:30:25 +0330

    feat(app): add application lifespan and clarify dependency lifecycle with yield
    - introduce lifespan using asynccontextmanager for startup/shutdown handling
    - reinforce correct use of yield for resource lifecycle management
    - align app initialization with FastAPI async execution model

--- app/main.py
+++ app/main.py
@@ -3,15 +3,22 @@ from typing import Dict
 import logging
 from app.logging import setup_logging
 from app.settings import settings
-
+from contextlib import asynccontextmanager
 
 logger = logging.getLogger(__name__)
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info('Application startup')
+    yield
+    logger.info('Application shutdown')
+
+
 def create_app() -> FastAPI:
     setup_logging(settings.debug)
-    logger.info("App starting ... APP_NAME: %r  DEBUG: %r ",settings.app_name,settings.debug)
 
-    app = FastAPI(title=settings.app_name,debug=settings.debug)
+    app = FastAPI(lifespan=lifespan,title=settings.app_name)
 
     @app.get("/health")
     def health() -> Dict[str,str]:
